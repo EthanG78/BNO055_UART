898f648cec33d7e61ad63b77328afb9424355f00
EthanG78 <ethangarnier78@gmail.com>
2023-01-25 14:30:21 -0400

    Updated test to actually run bno_init

--- examples/bno055_uart_test.c
+++ examples/bno055_uart_test.c
@@ -4,8 +4,16 @@
  * Author: Ethan Garnier
  */
 #include <stdio.h>
+#include <bno055_uart.h>
 
 int main()
 {
+    if (int bno_init("/dev/serial0", OPERATION_MODE_NDOF) == -1)
+    {
+        return 1
+    }
+
+    fprintf(stdout, "SUCCESSFULLY INITIALIZED BNO055\n");
+
     return 0;
 }
